cb53bbff048db98ac83f4f4b40179b6cbf933ae2
X1 Carbon <yjl9903@vip.qq.com>
2020-03-28 23:59:04 +0800

    remove: monaco json

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
   configureWebpack: {
     plugins: [
       new MonacoEditorPlugin({
-        languages: ['json', 'rust'],
+        languages: ['rust'],
         features: []
       })
     ]
